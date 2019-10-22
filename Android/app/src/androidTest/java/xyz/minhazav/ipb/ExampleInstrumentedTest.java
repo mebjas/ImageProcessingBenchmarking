07bb5918f0a93dfa4c25423a9d989dddc128d60d
Minhaz <minhazav@gmail.com>
2019-10-22 20:59:33 +0800

    Added benchmark code

--- Android/app/src/androidTest/java/xyz/minhazav/ipb/ExampleInstrumentedTest.java
+++ Android/app/src/androidTest/java/xyz/minhazav/ipb/ExampleInstrumentedTest.java
@@ -1,26 +1,28 @@
 package xyz.minhazav.ipb;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
+import androidx.benchmark.BenchmarkState;
+import androidx.benchmark.junit4.BenchmarkRule;
+import androidx.test.runner.AndroidJUnit4;
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+    @Rule
+    public BenchmarkRule benchmarkRule = new BenchmarkRule();
 
-        assertEquals("xyz.minhazav.ipb", appContext.getPackageName());
+    @Test
+    public void myBenchmark() {
+        final BenchmarkState state = benchmarkRule.getState();
+        while (state.keepRunning()) {
+            int k = 0;
+            for (int i = 0; i < 100000; i++) {
+                int l = k;
+                l = l + 1;
+                k = l;
+            }
+        }
     }
 }
